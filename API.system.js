cd2cfb808b599e0c79377194dc593ad574096a1b
iki1uc <entertrainer@web.de>
2026-08-03 10:16:30 +0200

    Update API.system.js

--- API.system.js
+++ API.system.js
@@ -1,9 +1,9 @@
-// API.system.js
 window.API_SYSTEM = {
-    idMode: () => ID_UNIT.mode,
-    idLevel: () => ID_UNIT.level,
-    idVector: () => ID_UNIT.vector
+    seed: () => ID_UNIT.seed,
+    triad: () => ID_UNIT.triad,
+    compat: () => ID_UNIT.compat
 };
+
 async function loadCSV() {
     const r = await fetch("schnittstelle-bandbreite-6.csv");
     const t = await r.text();
